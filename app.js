3af82407bbdea2a4a850673454a34df8e2a445c4
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-08-04 17:11:36 -0400

    Criando a lógica da função de sortear amigos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-let amigo = []; // Lista de amigos
+let amigos = []; // Lista de amigos
 
 function adicionarAmigo(){
     let nome = document.querySelector("#amigo").value;
@@ -8,7 +8,7 @@ function adicionarAmigo(){
     if(nome === ""){
         alert("Por favor, insira um nome.");
     } else{
-        amigo.push(nome);
+        amigos.push(nome);
 
         // console.log(amigo); // Verifica se o nome foi adicionado corretamente
         document.querySelector("#amigo").value = ""; //Limpa o campo de entrada do input
@@ -21,13 +21,24 @@ function atualizarListaAmigos() {
     let listaDeAmigos = document.querySelector("#listaAmigos"); // Lista do HTML
     listaDeAmigos.innerHTML = ""; // Limpa a lista antes de adicionar novos elementos.
 
-    for(let i = 0; i < amigo.length; i++){
+    for(let i = 0; i < amigos.length; i++){
         let itemDaLista = document.createElement("li"); // Cria um novo item da lista de amigos
-        itemDaLista.textContent = amigo[i];
+        itemDaLista.textContent = amigos[i];
         listaDeAmigos.appendChild(itemDaLista); // Adiciona o item à lista de amigos
     }
 }
 
 function sortearAmigo() {
+    // Condição para verificar se a lista de amigos está vazia
+    if (amigos.length === 0){
+        alert('A lista de amigos está vazia!');
+    } else{
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
 
+        let amigoSorteado = amigos[indiceAleatorio];
+        //console.log(amigoSorteado); // Verificar quem foi sorteado
+
+        let resultado = document.getElementById("resultado"); //Lista do HTML
+        resultado.innerHTML = `O amigo secreto sorteado é: ${amigoSorteado}`;
+    }
 }
