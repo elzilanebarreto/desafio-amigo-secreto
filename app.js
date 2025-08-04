8ca602980f75d910a2e3113d3a043ae25900d6dc
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-08-04 16:21:07 -0400

    Adicionando nomes na lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigo = [];
+
+function adicionarAmigo(){
+    let nome = document.querySelector("#amigo").value;
+
+    if(nome === ""){
+        alert("Por favor, insira um nome.");
+    } else{
+        amigo.push(nome);
+
+        console.log(amigo); //Verifica se o nome foi adicionado corretamente
+        document.querySelector("#amigo").value = ""; //Limpa o campo de entrada após adicionar o nome
+    }
+}
+
+function sortearAmigo() {
+
+}
